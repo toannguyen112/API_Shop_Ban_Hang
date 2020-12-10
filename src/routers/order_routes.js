322e8606c597d588b81f49ae87b0bca03b97571a
toannguyen112 <nguyencongtoana1nhvt@gmail.com>
2020-12-10 17:34:26 +0700

    update

--- src/routers/order_routes.js
+++ src/routers/order_routes.js
@@ -1,54 +1,53 @@
-const express = require('express');
-const auth_admin = require('../middleware/auth_admin');
-const Order = require('../models/order');
-const Cart = require('../models/cart');
-const auth_costomer = require('../middleware/auth_costomer');
+const express = require("express");
+const auth_admin = require("../middleware/auth_admin");
+const Order = require("../models/order");
+const Cart = require("../models/cart");
+const auth_costomer = require("../middleware/auth_costomer");
 
 const router = express.Router();
 
-
-// add order 
-router.post('/order', auth_costomer, async (req, res) => {
+// add order
+router.post("/order", auth_costomer, async (req, res) => {
     const reqOrder = req.body;
     try {
-
-
-
         const cart = await Cart.find({});
         if (!cart.length) {
             return res.status(404).send("No Products In Cart");
         }
-        const order = new Order({ ...reqOrder, list_product: cart, owner: req.costomer._id });
+        const order = new Order({
+            ...reqOrder,
+            list_product: cart,
+            owner: req.costomer._id,
+        });
         await order.save();
         await Cart.deleteMany({});
         res.status(200).send(order);
     } catch (error) {
         res.status(400).send();
     }
-})
+});
 
-// get order costomer 
-router.get('/order/costomer', auth_costomer, async (req, res) => {
-    const listOrderCostomer = await Order.find({ _id: req.costomer._id });
+// get order costomer
+router.get("/order/costomer", auth_costomer, async (req, res) => {
+    const listOrderCostomer = await Order.findById({ _id: req.costomer._id });
     res.status(200).send(listOrderCostomer);
-})
+});
 
 // get all list order
-router.get('/order', auth_admin, async (req, res) => {
+router.get("/order", auth_admin, async (req, res) => {
     const orderList = await Order.find({});
     res.status(200).send(orderList);
-})
+});
 
-// delete all order 
+// delete all order
 
-router.delete('/order', auth_admin, async (req, res) => {
+router.delete("/order", auth_admin, async (req, res) => {
     await Order.remove({});
     res.status(200).send("Delete Orders Success");
 });
 
-// delete  order item 
-router.delete('/order/:id', auth_admin, async (req, res) => {
-
+// delete  order item
+router.delete("/order/:id", auth_admin, async (req, res) => {
     try {
         const order = await Order.findByIdAndDelete(req.params.id);
         res.status(200).send(order);
@@ -57,7 +56,7 @@ router.delete('/order/:id', auth_admin, async (req, res) => {
     }
 });
 
-// update active 
+// update active
 
 router.put("/order/:id", auth_admin, async (req, res) => {
     const activeOrder = req.body.active;
@@ -65,10 +64,12 @@ router.put("/order/:id", auth_admin, async (req, res) => {
     console.log(activeOrder);
     console.log(orderId);
 
-    const order = await Order.findByIdAndUpdate({ _id: orderId }, { active: activeOrder });
+    const order = await Order.findByIdAndUpdate(
+        { _id: orderId },
+        { active: activeOrder }
+    );
     order.save();
     res.status(200).send(order);
-})
-
+});
 
 module.exports = router;
